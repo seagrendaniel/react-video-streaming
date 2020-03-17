e084af08ae5c62cb8980dab989e8c8a2707478ab
blackdanamite <seagrendaniel562@gmail.com>
2020-03-17 15:52:40 -0700

    add header component and wire up links

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './Streams/StreamCreate';
 import StreamEdit from './Streams/StreamEdit';
 import StreamList from './Streams/StreamList';
 import StreamDelete from './Streams/StreamDelete';
 import StreamShow from './Streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
